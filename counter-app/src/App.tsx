5a10d76fe812fb11ce3062cfde4342edef3851e9
Morisaki Mikiya <git@morisakimikiya.com>
2021-09-24 23:53:17 +0900

    Add increment

--- counter-app/src/App.tsx
+++ counter-app/src/App.tsx
@@ -1,12 +1,21 @@
 import { Center, VStack, Text } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
+import { useControllableState } from "@chakra-ui/hooks";
 
 function App() {
+  const [count, setCount] = useControllableState({ defaultValue: 0 });
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <Center>
       <VStack m={5} p={5} bg={"blue.100"} borderRadius="md">
-        <Text>0</Text>
-        <Button colorScheme="blue">+1</Button>
+        <Text>{count}</Text>
+        <Button colorScheme="blue" onClick={incrementCount}>
+          +1
+        </Button>
       </VStack>
     </Center>
   );
