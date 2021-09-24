6e56807cd3c8943fffdb3928471b17b15a1dc925
Morisaki Mikiya <git@morisakimikiya.com>
2021-09-25 02:11:06 +0900

    Add multiple counter feature

--- counter-app/src/App.tsx
+++ counter-app/src/App.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+
 import { Center } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
 
 import Counter from "./Counter";
 
 function App() {
+  const [counterNum, setCounterNum] = useState(1);
+  const [counters, setCounters] = useState([
+    <Counter name={"Counter " + counterNum} />,
+  ]);
+
+  // BUG: couterNum 1, 1, 2, 3, ....
+  const addCounter = () => {
+    setCounterNum(counterNum + 1);
+    const counter = <Counter name={"Counter " + counterNum} />;
+    const newCounters = [...counters, counter];
+    setCounters(newCounters);
+  };
+
   return (
     <Center>
-      <Counter name="Counter 1" />
+      {counters.map((counter) => counter)}
+      <Button colorScheme="teal" borderRadius="50%" onClick={addCounter}>
+        +
+      </Button>
     </Center>
   );
 }
