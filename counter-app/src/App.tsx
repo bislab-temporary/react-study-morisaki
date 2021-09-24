9f4b42bd633ce305cf118af203c5062c4362ff41
Morisaki Mikiya <git@morisakimikiya.com>
2021-09-24 22:48:50 +0900

    Modified button position to center

--- counter-app/src/App.tsx
+++ counter-app/src/App.tsx
@@ -1,7 +1,12 @@
+import { Container } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 
 function App() {
-  return <Button colorScheme="blue">+1</Button>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Button colorScheme="blue">+1</Button>
+    </Container>
+  );
 }
 
 export default App;
