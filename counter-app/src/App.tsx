cf5ab018098fa2f8b5bb3b99f48f44df26ed1729
Morisaki Mikiya <git@morisakimikiya.com>
2021-09-25 00:24:41 +0900

    Add decrement & color

--- counter-app/src/App.tsx
+++ counter-app/src/App.tsx
@@ -1,9 +1,13 @@
 import { Center, VStack, Text } from "@chakra-ui/layout";
-import { Button } from "@chakra-ui/button";
+import { Button, ButtonGroup } from "@chakra-ui/button";
 import { useControllableState } from "@chakra-ui/hooks";
 
 function App() {
   const [count, setCount] = useControllableState({ defaultValue: 0 });
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
 
   const incrementCount = () => {
     setCount(count + 1);
@@ -11,11 +15,16 @@ function App() {
 
   return (
     <Center>
-      <VStack m={5} p={5} bg={"blue.100"} borderRadius="md">
-        <Text>{count}</Text>
-        <Button colorScheme="blue" onClick={incrementCount}>
-          +1
-        </Button>
+      <VStack m={5} p={5} borderRadius="md">
+        <Text fontSize="2xl">{count}</Text>
+        <ButtonGroup variant="outline">
+          <Button colorScheme="red" onClick={decrementCount}>
+            -1
+          </Button>
+          <Button colorScheme="blue" onClick={incrementCount}>
+            +1
+          </Button>
+        </ButtonGroup>
       </VStack>
     </Center>
   );
