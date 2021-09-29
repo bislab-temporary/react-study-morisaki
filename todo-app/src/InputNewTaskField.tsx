300e0e5ffbc52b1e2c5ae5167cc1b90dd9f777a5
Morisaki Mikiya <git@morisakimikiya.com>
2021-09-30 08:55:14 +0900

    style: 「追加」のイメージにより近いため
    タスクの追加ボタンのアイコンを変更(✔ → ＋)

--- todo-app/src/InputNewTaskField.tsx
+++ todo-app/src/InputNewTaskField.tsx
@@ -1,4 +1,4 @@
-import { CheckIcon } from "@chakra-ui/icons";
+import { AddIcon } from "@chakra-ui/icons";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import { useState } from "react";
 import { iTask } from "./App";
@@ -48,7 +48,7 @@ const InputNewTaskField = ({ tasks, setTasks }: Props) => {
         }}
       />
       <InputRightElement
-        children={<CheckIcon color="green.500" />}
+        children={<AddIcon color="green.500" />}
         _hover={{ bg: "green.100", borderRadius: "50%" }}
         onClick={onSubmitTask}
       />
