453bbc2b5517dc90097313570124eca8fc6b45e7
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-04 21:37:20 +0900

    fix: DB になぜか1ヶ月後の timestamp で追加されるため

--- todo-app/src/models/populate.ts
+++ todo-app/src/models/populate.ts
@@ -3,17 +3,17 @@ import { db } from "./db";
 export const populate = async () => {
   db.tasksTable.bulkAdd([
     {
-      createdAt: new Date(2021, 9, 30, 1, 0, 0).getTime(),
+      createdAt: 0,
       isDone: false,
       text: "Task1",
     },
     {
-      createdAt: new Date(2021, 9, 30, 2, 0, 0).getTime(),
+      createdAt: 1,
       isDone: false,
       text: "Task2",
     },
     {
-      createdAt: new Date(2021, 9, 30, 3, 0, 0).getTime(),
+      createdAt: 2,
       isDone: false,
       text: "Task3",
     },
