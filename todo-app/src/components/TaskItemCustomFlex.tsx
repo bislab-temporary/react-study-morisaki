ec32ad540072b7ca592253f57981b54f39ef6141
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-04 17:10:58 +0900

    fix: タスク編集時にも打ち消し線が入っている問題を解決するため
    打ち消し線を適用するコンポーネントを Flex から EditablePreview に変更

--- todo-app/src/components/TaskItemCustomFlex.tsx
+++ todo-app/src/components/TaskItemCustomFlex.tsx
@@ -1,27 +1,17 @@
 import { Flex, FlexProps } from "@chakra-ui/layout";
 
 type CustomProps = {
-  isDone: boolean;
   isDraggingOver: boolean;
 };
 
 type Props = FlexProps & CustomProps;
 
-const TaskItemCustomFlex = ({
-  isDone,
-  isDraggingOver,
-  ...flexProps
-}: Props) => {
-  if (isDone) {
-    flexProps["color"] = "gray";
-    flexProps["as"] = "s";
-  }
-  if (isDraggingOver) {
-    flexProps["borderWidth"] = 2;
-    flexProps["borderColor"] = "blue.500";
-    flexProps["bg"] = "blue.200";
-  }
-  return <Flex {...flexProps} />;
+const TaskItemCustomFlex = ({ isDraggingOver, ...flexProps }: Props) => {
+  return isDraggingOver ? (
+    <Flex {...flexProps} borderWidth={2} borderColor="blue.500" bg="blue.200" />
+  ) : (
+    <Flex {...flexProps} />
+  );
 };
 
 export default TaskItemCustomFlex;
